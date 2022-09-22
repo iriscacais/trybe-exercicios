6decb427e4dd69b8ae57aa5a8912b07712105ec4
iriscacais <mandeparairis@gmail.com>
2022-09-22 20:32:46 -0300

    atualiza dia 01

---
+++ fundamentos/secao-04-JS/dia-01-introducao-JS/programa13.js
@@ -0,0 +1,13 @@
+// Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+//Bonus: use somente um if.
+
+
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+if (num1%2!=0 || num2%2!= 0 || num3%2!=0){
+    console.log (true)
+} else {
+    console.log (false)
+}
