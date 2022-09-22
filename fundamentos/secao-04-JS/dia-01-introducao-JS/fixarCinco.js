7173f78cc6c9f57646e6fd86b0be6fe82cd738b8
iriscacais <mandeparairis@gmail.com>
2022-09-21 21:46:26 -0300

    exercicios para fixar 4,5 e 6

---
+++ fundamentos/secao-04-JS/dia-01-introducao-JS/fixarCinco.js
@@ -0,0 +1,8 @@
+let weekDay = 'sexta-feira';
+
+if (weekDay = 'segunda-feira' | 'terça-feira' | 'quarta-feira' |  'quinta-feira' |'sexta-feira') {
+    console.log ('Oba, mais um dia de aprendizado na Trybe')
+}
+else if (weekDay = 'sabado' |'domingo'){ 
+    console.log ('FINALMENTE, descanso merecido UwU')
+}
