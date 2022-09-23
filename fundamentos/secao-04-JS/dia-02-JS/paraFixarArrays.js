05690f3d9139f43571822f575a5f4b1e655f08d8
iriscacais <mandeparairis@gmail.com>
2022-09-22 21:07:51 -0300

    exercícios para fixar, array, for e forOf

---
+++ fundamentos/secao-04-JS/dia-02-JS/paraFixarArrays.js
@@ -0,0 +1,20 @@
+//Obtenha o valor “Serviços” do array menu:
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu1 [1];
+
+console.log(menuServices);
+
+//Procure o índice do valor “Portfólio” do array menu:
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2.indexOf ('Portfólio');
+
+console.log(indexOfPortfolio)
+
+//Adicione o valor “Contato” no final do array menu:
+//De olho na dica 👀: Use o método .push()
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push('Contato');
+console.log(menu3);
