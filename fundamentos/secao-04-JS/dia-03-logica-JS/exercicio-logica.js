fc7d68f30049584d5c30d107e0267e866a538878
iriscacais <mandeparairis@gmail.com>
2022-09-25 22:34:56 -0300

    atualiza exercicios fatorial

---
+++ fundamentos/secao-04-JS/dia-03-logica-JS/exercicio-logica.js
@@ -0,0 +1,8 @@
+let valorInicial = 5;
+let fatorialDez;
+let listaDecremento = [];
+
+for ( let index= 1; index <= valorInicial; index +=1){
+listaDecremento.push([index]);
+}
+console.log(listaDecremento)
